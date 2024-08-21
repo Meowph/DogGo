1731554a694538d5482435ae9a72df6de000c297
Meowph <bpardue33@gmail.com>
2024-08-21 15:53:41 -0400

    "Part 1 Complete... working on Part 2"

---
+++ Repositiories/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+        void AddOwner(Owner entry);
+        void UpdateOwner(Owner entry);
+        void DeletOwner(int id);
+    }
+}
